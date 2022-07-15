f0dbc332f36842b9cf974290e18c52531e384cab
shoma <shoma0321prgm@gmail.com>
2022-07-15 18:46:58 +0900

    センサー機能設置

--- MotionSensor/ViewController.swift
+++ MotionSensor/ViewController.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    let motionManager = CMMotionManager()
+    
+    @IBOutlet weak var fish: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let originalX = fish.center.x//fishの最初のx座標を入れておく
+        
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.deviceMotionUpdateInterval = 0.02
+            motionManager.startDeviceMotionUpdates(to: OperationQueue.current!) { motion, error in
+                print(motion!.attitude.roll)//iphoneの縦方向を軸とした傾き -ΠからΠまで
+                
+                let newX = originalX + CGFloat(motion!.attitude.roll * 100)
+                self.fish.center = CGPoint(x: newX, y: self.fish.center.y)
+            }
+        }
     }
 
 
